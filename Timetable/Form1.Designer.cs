6aeb023a09eb4466d7ad08ac3ddc80d4d837e0bc
Анастасия <arkalikova@edu.hse.ru>
2018-01-16 00:26:35 +0500

    init

---
+++ Timetable/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Timetable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDoStuff = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDoStuff
+            // 
+            this.btnDoStuff.Location = new System.Drawing.Point(12, 12);
+            this.btnDoStuff.Name = "btnDoStuff";
+            this.btnDoStuff.Size = new System.Drawing.Size(131, 23);
+            this.btnDoStuff.TabIndex = 0;
+            this.btnDoStuff.Text = "Скопировать файл";
+            this.btnDoStuff.UseVisualStyleBackColor = true;
+            this.btnDoStuff.Click += new System.EventHandler(this.btnDoStuff_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(155, 47);
+            this.Controls.Add(this.btnDoStuff);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDoStuff;
+    }
+}
+
---
+++ Timetable/Form1.cs
@@ -0,0 +1,67 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timetable
+{
+    public partial class Form1 : Form
+    {
+        //пути к файлам
+        String fromPath = @"C:\Users\Анастасия\Downloads\Telegram Desktop\17Vz_17Uz_s_30_10_2017_po_12_11_2017_utv_24_10_2017.xlsx";
+        String toPath = @"C:\Users\Анастасия\Desktop\Table_To_Object.xlsx";
+
+        //Напиши свои, а мои закомментируй
+        //String fromPath = @"C:\Users\Анастасия\Downloads\Telegram Desktop\17Vz_17Uz_s_30_10_2017_po_12_11_2017_utv_24_10_2017.xlsx";
+        //String toPath = @"C:\Users\Анастасия\Desktop\Table_To_Object.xlsx";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDoStuff_Click(object sender, EventArgs e)
+        {
+            var fi_from = new FileInfo(fromPath);
+            var fi_to = new FileInfo(toPath);
+            
+            //удаляем каждый раз результирующий файл
+            if (File.Exists(toPath))
+                File.Delete(toPath);
+
+            using (var package = new ExcelPackage(fi_from))
+            {
+                var workbook = package.Workbook;
+                using (var result = new ExcelPackage(fi_to))
+                {
+                    foreach (var worksheet in workbook.Worksheets)
+                    {
+                        //копируем все листы с содержимым
+                        var new_worksheet = result.Workbook.Worksheets.Add(worksheet.Name, worksheet);
+                        int start_row = new_worksheet.Dimension.Start.Row;
+                        int end_row = new_worksheet.Dimension.End.Row;
+                        int start_col = new_worksheet.Dimension.Start.Column;
+                        int end_col = new_worksheet.Dimension.End.Column;
+                        for (int i = start_row; i < end_row; i++)
+                        {
+                            for (int j = start_col; j < end_col; j++)
+                            {
+                                //можно что-нибудь поделать
+                            }
+                        }
+                    }
+
+                    result.Save();
+                }
+                
+            }
+        }
+    }
+}
